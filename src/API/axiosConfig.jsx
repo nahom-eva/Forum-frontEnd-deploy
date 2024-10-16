bb452d218989c31313634bf741ab2540d7f78e67
unknown <ntade499@gmail.com>
2024-10-16 22:11:08 +0300

    epdate

--- src/API/axiosConfig.jsx
+++ src/API/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadi-forum-backend-deploy-1-05zz.onrender.com/api",
+  baseURL: "https://forum-backend-deploy.onrender.com",
 });
 
 export default axiosBase;

629a248923aef62bb0eae55fffce31a1ea681578
unknown <ntade499@gmail.com>
2024-10-16 22:23:27 +0300

    update

--- src/API/axiosConfig.jsx
+++ src/API/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://forum-backend-deploy.onrender.com",
+  baseURL: "https://forum-backend-deploy.onrender.com/api",
 });
 
 export default axiosBase;
